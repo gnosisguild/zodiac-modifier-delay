f8bcb74fad122ed4be85054bbbe2aa7bbb6a95e9
cbrzn <cesarbrazon10@gmail.com>
2021-09-04 20:58:15 +0200

    chore: updated zodiac package name

--- contracts/Delay.sol
+++ contracts/Delay.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.8.0;
 
-import "@gnosis/zodiac/contracts/core/Modifier.sol";
+import "@zodiacdao/zodiac/contracts/core/Modifier.sol";
 
 contract Delay is Modifier {
     event DelaySetup(address indexed initiator, address indexed avatar);
