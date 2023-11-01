6dd6b2b0f4fa33f8e4605c5a8a279f4d8409ad18
cristovaoth <cristovao.honorato@gmail.com>
2023-11-01 13:36:38 +0100

    Refresh dependencies, and package.json scripts

--- .solcover.js
+++ .solcover.js
@@ -1,7 +1,3 @@
 module.exports = {
-    skipFiles: ['test/Imports.sol', 'test/TestAvatar.sol', 'test/Mock.sol'],
-    mocha: {
-        grep: "@skip-on-coverage", // Find everything with this tag
-        invert: true               // Run the grep's inverse set.
-    }
+  skipFiles: ["test"],
 };
