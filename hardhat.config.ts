cf77ce4681247eb045d7a156808faaf03c8fb18d
cristovaoth <cristovao.honorato@gmail.com>
2023-11-01 13:36:38 +0100

    Refresh dependencies, and package.json scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,82 +1,124 @@
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "solidity-coverage";
-import "hardhat-deploy";
-import dotenv from "dotenv";
-import type { HttpNetworkUserConfig } from "hardhat/types";
-import yargs from "yargs";
+import '@nomicfoundation/hardhat-toolbox'
+import 'hardhat-gas-reporter'
+import 'hardhat-deploy'
 
-const argv = yargs
-  .option("network", {
-    type: "string",
-    default: "hardhat",
-  })
-  .help(false)
-  .version(false).argv;
+import './src/deploy/verify'
 
-// Load environment variables.
-dotenv.config();
-const { INFURA_KEY, MNEMONIC, ETHERSCAN_API_KEY, PK } = process.env;
+import dotenv from 'dotenv'
+import { HttpNetworkUserConfig } from 'hardhat/types'
+import { DeterministicDeploymentInfo } from 'hardhat-deploy/dist/types'
+import { getSingletonFactoryInfo } from '@safe-global/safe-singleton-factory'
 
-import "./src/tasks/setup";
+dotenv.config()
 
-const DEFAULT_MNEMONIC =
-  "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+const { INFURA_KEY, MNEMONIC, ETHERSCAN_API_KEY } = process.env
 
-const sharedNetworkConfig: HttpNetworkUserConfig = {};
-if (PK) {
-  console.log("yah")
-  sharedNetworkConfig.accounts = [PK];
-} else {
-  sharedNetworkConfig.accounts = {
-    mnemonic: MNEMONIC || DEFAULT_MNEMONIC,
-  };
-}
-
-if (["rinkeby", "mainnet"].includes(argv.network) && INFURA_KEY === undefined) {
-  throw new Error(
-    `Could not find Infura key in env, unable to connect to network ${argv.network}`,
-  );
+const sharedNetworkConfig: HttpNetworkUserConfig = {
+  accounts: {
+    mnemonic:
+      MNEMONIC ||
+      'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat',
+  },
 }
 
 export default {
   paths: {
-    artifacts: "build/artifacts",
-    cache: "build/cache",
-    deploy: "src/deploy",
-    sources: "contracts",
+    artifacts: 'build/artifacts',
+    cache: 'build/cache',
+    deploy: 'src/deploy',
+    sources: 'contracts',
   },
   solidity: {
-    compilers: [
-      { version: "0.8.0" },
-      { version: "0.6.12" },
-    ]
+    compilers: [{ version: '0.8.20' }],
   },
+  defaultNetwork: 'hardhat',
   networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+      blockGasLimit: 100000000,
+      gas: 100000000,
+    },
     mainnet: {
       ...sharedNetworkConfig,
       url: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+    },
+    gnosis: {
+      ...sharedNetworkConfig,
+      url: 'https://rpc.gnosischain.com',
+    },
+    ewc: {
+      ...sharedNetworkConfig,
+      url: `https://rpc.energyweb.org`,
+    },
+    goerli: {
+      ...sharedNetworkConfig,
+      url: `https://goerli.infura.io/v3/${INFURA_KEY}`,
+    },
+    mumbai: {
+      ...sharedNetworkConfig,
+      url: `https://polygon-mumbai.infura.io/v3/${INFURA_KEY}`,
+    },
+    polygon: {
+      ...sharedNetworkConfig,
+      url: `https://polygon-mainnet.infura.io/v3/${INFURA_KEY}`,
+    },
+    volta: {
+      ...sharedNetworkConfig,
+      url: `https://volta-rpc.energyweb.org`,
+    },
+    bsc: {
+      ...sharedNetworkConfig,
+      url: `https://bsc-dataseed.binance.org/`,
+    },
+    arbitrum: {
+      ...sharedNetworkConfig,
+      url: `https://arb1.arbitrum.io/rpc`,
+    },
+    fantomTestnet: {
+      ...sharedNetworkConfig,
+      url: `https://rpc.testnet.fantom.network/`,
+    },
+    avalanche: {
+      ...sharedNetworkConfig,
+      url: `https://api.avax.network/ext/bc/C/rpc`,
     },
     rinkeby: {
       ...sharedNetworkConfig,
       url: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
     },
-    xdai: {
-      ...sharedNetworkConfig,
-      url: "https://xdai.poanetwork.dev",
-    },
     matic: {
       ...sharedNetworkConfig,
-      url: "https://rpc-mainnet.maticvigil.com"
-    }
+      url: 'https://rpc-mainnet.maticvigil.com',
+    },
   },
+  deterministicDeployment,
   namedAccounts: {
     deployer: 0,
-  },
-  mocha: {
-    timeout: 2000000,
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
-};
+  gasReporter: {
+    enabled: true,
+  },
+}
+
+function deterministicDeployment(network: string): DeterministicDeploymentInfo {
+  const info = getSingletonFactoryInfo(parseInt(network))
+  if (!info) {
+    throw new Error(`
+      Safe factory not found for network ${network}. You can request a new deployment at https://github.com/safe-global/safe-singleton-factory.
+      For more information, see https://github.com/safe-global/safe-contracts#replay-protection-eip-155
+    `)
+  }
+
+  const gasLimit = BigInt(info.gasLimit)
+  const gasPrice = BigInt(info.gasPrice)
+
+  return {
+    factory: info.address,
+    deployer: info.signerAddress,
+    funding: String(gasLimit * gasPrice),
+    signedTx: info.transaction,
+  }
+}
