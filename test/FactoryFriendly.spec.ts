ffc57fce86a5d8ca68dc11b11bd15e0da8de67b0
cristovaoth <cristovao.honorato@gmail.com>
2023-11-01 16:01:54 +0100

    Migrate FactoryFriendly tests

--- test/FactoryFriendly.spec.ts
+++ test/FactoryFriendly.spec.ts
@@ -1,77 +1,87 @@
-import { expect } from "chai";
-import hre, { deployments, ethers } from "hardhat";
-import "@nomiclabs/hardhat-ethers";
-import { AbiCoder } from "ethers/lib/utils";
-import { AddressOne } from "@gnosis.pm/safe-contracts";
+import { expect } from 'chai'
+import hre, { ethers } from 'hardhat'
+import { loadFixture } from '@nomicfoundation/hardhat-network-helpers'
+import { AddressOne } from '@gnosis.pm/safe-contracts'
+import { AbiCoder, keccak256, toUtf8Bytes } from 'ethers'
+import { Delay__factory } from '../typechain-types'
 
-const FirstAddress = "0x0000000000000000000000000000000000000001";
-const saltNonce = "0xfa";
+const FirstAddress = '0x0000000000000000000000000000000000000001'
 
-describe("Module works with factory", () => {
-  const cooldown = 100;
-  const expiration = 180;
-  const paramsTypes = ["address", "address", "address", "uint256", "uint256"];
+describe('Module works with factory', () => {
+  const cooldown = 100
+  const expiration = 180
+  const paramsTypes = ['address', 'address', 'address', 'uint256', 'uint256']
 
-  const baseSetup = deployments.createFixture(async () => {
-    await deployments.fixture();
-    const Factory = await hre.ethers.getContractFactory("ModuleProxyFactory");
-    const DelayModifier = await hre.ethers.getContractFactory("Delay");
-    const factory = await Factory.deploy();
+  async function setup() {
+    const Factory = await hre.ethers.getContractFactory('ModuleProxyFactory')
+    const factory = await Factory.deploy()
 
-    const masterCopy = await DelayModifier.deploy(
+    const Delay = await hre.ethers.getContractFactory('Delay')
+
+    const masterCopy = await Delay.deploy(
       FirstAddress,
       FirstAddress,
       FirstAddress,
       0,
       0
-    );
+    )
 
-    return { factory, masterCopy };
-  });
-
-  it("should throw because master copy is already initialized", async () => {
-    const { masterCopy } = await baseSetup();
-    const encodedParams = new AbiCoder().encode(paramsTypes, [
+    const encodedParams = AbiCoder.defaultAbiCoder().encode(paramsTypes, [
       AddressOne,
       AddressOne,
       AddressOne,
-      100,
-      180,
-    ]);
+      cooldown,
+      expiration,
+    ])
 
-    await expect(masterCopy.setUp(encodedParams)).to.be.revertedWith(
-      "Initializable: contract is already initialized"
-    );
-  });
+    return { factory, masterCopy, encodedParams }
+  }
 
-  it("should deploy new amb module proxy", async () => {
-    const { factory, masterCopy } = await baseSetup();
-    const [avatar, owner, target] = await ethers.getSigners();
+  it('should throw because master copy is already initialized', async () => {
+    const { masterCopy, encodedParams } = await loadFixture(setup)
+
+    await expect(masterCopy.setUp(encodedParams)).to.be.revertedWithCustomError(
+      masterCopy,
+      'InvalidInitialization'
+    )
+  })
+
+  it('should deploy new amb module proxy', async () => {
+    const { factory, masterCopy } = await loadFixture(setup)
+    const [owner, avatar, target] = await ethers.getSigners()
     const paramsValues = [
       owner.address,
       avatar.address,
       target.address,
       100,
       180,
-    ];
-    const encodedParams = [new AbiCoder().encode(paramsTypes, paramsValues)];
-    const initParams = masterCopy.interface.encodeFunctionData(
-      "setUp",
-      encodedParams
-    );
-    const receipt = await factory
-      .deployModule(masterCopy.address, initParams, saltNonce)
-      .then((tx: any) => tx.wait());
+    ]
+    const setupParams = AbiCoder.defaultAbiCoder().encode(
+      paramsTypes,
+      paramsValues
+    )
+    const initializer = masterCopy.interface.encodeFunctionData('setUp', [
+      setupParams,
+    ])
+    const receipt = await (
+      await factory.deployModule(masterCopy, initializer, 0)
+    ).wait()
 
-    // retrieve new address from event
-    const {
-      args: [newProxyAddress],
-    } = receipt.events.find(
-      ({ event }: { event: string }) => event === "ModuleProxyCreation"
-    );
+    const eventAsTopic = keccak256(
+      toUtf8Bytes('ModuleProxyCreation(address,address)')
+    )
 
-    const newProxy = await hre.ethers.getContractAt("Delay", newProxyAddress);
-    expect(await newProxy.txCooldown()).to.be.eq(cooldown);
-    expect(await newProxy.txExpiration()).to.be.eq(expiration);
-  });
-});
+    const entry = receipt?.logs.find(
+      (entry) => entry.topics.length > 0 && entry.topics[0] == eventAsTopic
+    )
+
+    const [newProxyAddress] = AbiCoder.defaultAbiCoder().decode(
+      ['address'],
+      entry?.topics[1] as string
+    )
+
+    const delay = Delay__factory.connect(newProxyAddress, hre.ethers.provider)
+    expect(await delay.txCooldown()).to.be.eq(cooldown)
+    expect(await delay.txExpiration()).to.be.eq(expiration)
+  })
+})
