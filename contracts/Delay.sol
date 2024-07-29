fe5528abfabd7fc9fb79221fa13d52b93f9dc0d7
Cristóvão Honorato <cristovao.honorato@gmail.com>
2024-07-29 14:29:30 +0200

    Remove zodiac dependency

--- contracts/Delay.sol
+++ contracts/Delay.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.8.0;
 
-import {Enum, Modifier} from "@gnosis.pm/zodiac/contracts/core/Modifier.sol";
+import {Enum, Modifier} from "zodiac-core/contracts/core/Modifier.sol";
 
 contract Delay is Modifier {
     event DelaySetup(
--- contracts/test/TestFactory.sol
+++ contracts/test/TestFactory.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity >=0.8.0;
 
-import {ModuleProxyFactory} from "@gnosis.pm/zodiac/contracts/factory/ModuleProxyFactory.sol";
+import {ModuleProxyFactory} from "zodiac-core/contracts/factory/ModuleProxyFactory.sol";
 
 contract TestFactory is ModuleProxyFactory {}
